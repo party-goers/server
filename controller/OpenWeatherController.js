207a70a594cb8f0bb5f73c7c3649211c389ba38d
serafimsng <serafimsinaga@gmail.com>
2020-01-09 19:58:26 +0700

    weather description ok

--- controller/OpenWeatherController.js
+++ controller/OpenWeatherController.js
@@ -2,12 +2,22 @@ const axios = require('axios');
 
 class OpenWeatherController{
     static  cityWeather(req,res,next){
-        console.log(req.params)
+        const paramDate = req.params.date
         axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${req.params.cityname}&APPID=c2cbe47e5236ed05b5c314c584da9bef`)
         .then(({data})=>{
             if(data){
-                console.log(data)
-                res.json(data)
+                let weatherDesc=null
+                const allWeatherData = data.list
+                for(let weather of allWeatherData){
+                    let completeDate = weather.dt_txt
+                    let date = completeDate.split(' ')
+                    if(date[0]==paramDate){
+                        weatherDesc = weather.weather[0].description
+                    }
+                }
+                res.json({
+                    weather: weatherDesc
+                })
             }else{
                 res.json({
                     message: 'city name not found'
--- router/openweatherRouter.js
+++ router/openweatherRouter.js
@@ -2,7 +2,7 @@ const OpenWeatherController = require('../controller/OpenWeatherController')
 const router = require('express').Router()
 
 
-router.get('/:cityname', OpenWeatherController.cityWeather)
+router.get('/:cityname/:date', OpenWeatherController.cityWeather)
 
 
 module.exports = router
